105fc24603d8b1f17ccca14b2e267da783cf39ce
prameetc <prameet.c@gmail.com>
2019-04-16 16:02:14 +0530

    Updated DB

--- src/app.local.ts
+++ src/app.local.ts
@@ -1,6 +1,31 @@
 import app from "./app";
-const port = 3000;
+const port = 8080;
+const mongoClient = require("mongodb").MongoClient;
+
+const dbName = "bitnation";
+
+let db, Client;
 
 app.listen(port);
 
+new mongoClient(
+  "mongodb://8e02a03c-0ee0-4-231-b9ee.documents.azure.com:10255/?ssl=true",
+  {
+    auth: {
+      user: "8e02a03c-0ee0-4-231-b9ee",
+      password:
+        "hqdvE6k7s3uOMDaYGq0Ce1RETKzGhfKl60Dqj4Lp2cB8JphoPpiJWOgYaIwJcZEfHbJqmbDAJWUsY0gDrdkLug=="
+    },
+    useNewUrlParser: true
+  }
+).connect((err: any, client: any) => {
+  if (err) return console.error(err);
+  console.log("Database connected");
+  db = client.db(dbName);
+  exports.db = db;
+  exports.client = client;
+});
+
 console.log(`Hello World, listening to port ${port}`);
+
+
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,8 +1,13 @@
 import express from "express";
 let bodyParser = require("body-parser");
 let uuid = require("uuid");
+const bcrypt = require('bcrypt');
 
-var router = express.Router();
+let appLocal = require('../app.local');
+
+// const Client = require('../app.local');
+
+let router = express.Router();
 
 router.use(bodyParser.json());
 
@@ -11,66 +16,31 @@ router.use(bodyParser.urlencoded({extended: true}));
 const jwt = require("jsonwebtoken");
 
 router.get("/connect", (req, res) => {
-  const mongoClient = require("mongodb").MongoClient;
-  const assert = require("assert");
-
-  // Connection URL
-  const dbName = "bitnation";
-
-  // Use connect method to connect to the server
-
-  new mongoClient(
-    "mongodb://8e02a03c-0ee0-4-231-b9ee.documents.azure.com:10255/?ssl=true",
-    {
-      auth: {
-        user: "8e02a03c-0ee0-4-231-b9ee",
-        password:
-          "hqdvE6k7s3uOMDaYGq0Ce1RETKzGhfKl60Dqj4Lp2cB8JphoPpiJWOgYaIwJcZEfHbJqmbDAJWUsY0gDrdkLug=="
-      }
-    }
-  ).connect((err: any, client: any) => {
-    if (err) return console.error(err);
-    console.log("Database connected");
-    const db = client.db(dbName);
-    // db.createCollection("people", function(err: any, res: any) {
-    //   if (err) throw err;
-    //   console.log("Collection created!");
-    // });
-    // let myobj = { id: uuid.v4(), name: "Prameet", company: "bitnation" };
-    var query = { name: "Prameet" };
-    db.collection("people").find(query).toArray(function (err: any, result: any) {
-      if (err) throw err;
-      console.log(result);
-    });
-    // db.collection("people").insertOne(myobj, function(err: any, res: any) {
-    //   if (err) throw err;
-    //   console.log("1 document inserted");
-    // });
-    // db.collection("users").deleteOne(myobj, function (err: any, obj: any) {
-    //   if (err) throw err;
-    //   console.log("1 document deleted");
-    // });
-    client.close();
     return res.status(200).json({
       success: true,
     });
   });
-});
 
 router.post("/login", (req, res) => {
   let {email, password} = req.body;
   let opts: any = {};
-  if (email === "abc@def.com") {
-    if (password === "password") {
-      opts.expiresIn = 120; //token expires in 2min
-      const secret = "SECRET_KEY"; //normally stored in process.env.secret
-      const token = jwt.sign({email}, secret, opts);
-      return res.status(200).json({
-        success: true,
-        access_token: token
-      });
-    }
-  }
+  var query = { email: email };
+  appLocal.db.collection("people").find(query).toArray(function (err: any, result: any) {
+    if (err) throw err;
+    console.log(result);
+    bcrypt.compare(password, result[0].password, function (err: any, bcryptres: any) {
+      if (bcryptres) {
+        return res.json({
+          success: false,
+          access_token: jwt.sign({
+            exp: Math.floor(Date.now() / 1000) + (60 * 60),
+            data: email
+          }, result[0].password),
+        });
+      }
+    });
+  });
+
   return res.status(401).json({
     success: false,
     message: "Auth Failed"
@@ -78,12 +48,46 @@ router.post("/login", (req, res) => {
 });
 
 router.post("/signup", (req, res) => {
-  let {email, name, password} = req.body;
+  let { email, name, password, bitnationId } = req.body;
   let opts: any = {};
-  return res.status(200).json({
-    success: true,
-    message: "User Created Successfully"
+    // db.createCollection("people", function(err: any, res: any) {
+  //   if (err) throw err;
+  //   console.log("Collection created!");
+  // });
+  const saltRounds = 10;
+  // appLocal.db.collection('people', function (err: any, collection: any) {
+  //   collection.remove({}, function (err: any, removed: any) {
+  //     if (removed) {
+  //       console.log('collection removed');
+  //     } else {
+  //       console.log('error emoving collection');
+  //     }
+  //   });
+  // });
+  // bcrypt.hash(password, saltRounds, function (err: any, hash: any) {
+  //   const ecptPassword = hash;
+  //   let myobj = { email, name, password: ecptPassword, bitnationId };
+  //   appLocal.db.collection("people").insertOne(myobj, function (err: any, res: any) {
+  //     if (err) throw err;
+  //     console.log("1 document inserted");
+  //     return res.status(200).json({
+  //       success: true,
+  //       message: "User Created Successfully"
+  //     });
+  //   });
+  // });
+  // var query = { name: "Prameet" };
+  // appLocal.db.collection("people").find({}).toArray(function (err: any, result: any) {
+  //   if (err) throw err;
+  //   console.log(result);
+  // });
+  // db.collection("users").deleteOne(myobj, function (err: any, obj: any) {
+  //   if (err) throw err;
+  //   console.log("1 document deleted");
+  // });
+
+    
   });
-});
+
 
 module.exports = router;
