9ae86ec91958b2ff622a9561e6e87c9a3c42cf7c
prameetc <prameet.c@gmail.com>
2019-04-16 17:10:44 +0530

    Fixed Login and Sign Up

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -21,31 +21,51 @@ router.get("/connect", (req, res) => {
     });
   });
 
-router.post("/login", (req, res) => {
-  let {email, password} = req.body;
-  let opts: any = {};
-  var query = { email: email };
-  appLocal.db.collection("people").find(query).toArray(function (err: any, result: any) {
+router.get("/getUsers", (req, res) => {
+  appLocal.db.collection("people").find({}).toArray(function (err: any, users: any) {
     if (err) throw err;
-    console.log(result);
-    bcrypt.compare(password, result[0].password, function (err: any, bcryptres: any) {
-      if (bcryptres) {
-        return res.json({
-          success: false,
-          access_token: jwt.sign({
-            exp: Math.floor(Date.now() / 1000) + (60 * 60),
-            data: email
-          }, result[0].password),
-        });
-      }
+    users.map(function (item: any) {
+      delete item.password;
+      return item;
     });
-  });
-
-  return res.status(401).json({
-    success: false,
-    message: "Auth Failed"
+    const data = {
+      status: 200,
+      result: users
+    }
+    res.send(data);
   });
 });
+
+router.post("/login", async (req, res) => {
+  let { email, password } = req.body;
+  let opts: any = {};
+  var query = { email };
+  let data: any = {};
+  appLocal.db.collection("people").find({ email }).toArray(function (err: any, user: any) {
+    if (user.length > 0) {
+      bcrypt.compare(password, user[0].password, function (err: any, bcryptres: any) {
+        if (bcryptres) {
+          res.send({
+            status: "success",
+            access_token: jwt.sign({ email: email }, user[0].password, {
+              expiresIn: "10d"
+            }),
+          });
+        } else {
+          res.send({
+            status: "error",
+            message: "Password is incorrect",
+          });
+        }
+      });
+    } else {
+      res.send({
+            status: "error",
+            message: "User is not present",
+          });
+    }
+    });
+ });
 
 router.post("/signup", (req, res) => {
   let { email, name, password, bitnationId } = req.body;
@@ -64,23 +84,19 @@ router.post("/signup", (req, res) => {
   //     }
   //   });
   // });
-  // bcrypt.hash(password, saltRounds, function (err: any, hash: any) {
-  //   const ecptPassword = hash;
-  //   let myobj = { email, name, password: ecptPassword, bitnationId };
-  //   appLocal.db.collection("people").insertOne(myobj, function (err: any, res: any) {
-  //     if (err) throw err;
-  //     console.log("1 document inserted");
-  //     return res.status(200).json({
-  //       success: true,
-  //       message: "User Created Successfully"
-  //     });
-  //   });
-  // });
+  bcrypt.hash(password, saltRounds, function (err: any, hash: any) {
+    const ecptPassword = hash;
+    let myobj = { email, name, password: ecptPassword, bitnationId };
+    appLocal.db.collection("people").insertOne(myobj, function (err: any, res: any) {
+      if (err) throw err;
+      console.log("1 document inserted");
+      return res.status(200).json({
+        success: true,
+        message: "User Created Successfully"
+      });
+    });
+  });
   // var query = { name: "Prameet" };
-  // appLocal.db.collection("people").find({}).toArray(function (err: any, result: any) {
-  //   if (err) throw err;
-  //   console.log(result);
-  // });
   // db.collection("users").deleteOne(myobj, function (err: any, obj: any) {
   //   if (err) throw err;
   //   console.log("1 document deleted");
